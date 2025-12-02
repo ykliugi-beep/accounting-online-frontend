02287075eaf798dfc491eb65abe3294a62e029b5
Sasonal Dekant <sasonal.dekant@gmail.com>
2025-12-02 16:16:35 +0100

    fix: Change routing to show DocumentCreatePage instead of DocumentListPage for all document types

--- src/App.tsx
+++ src/App.tsx
@@ -60,46 +60,46 @@ function App() {
               {/* Dashboard */}
               <Route path="/" element={<DashboardPage />} />
               
-              {/* Documents */}
+              {/* Generic Documents */}
               <Route path="/documents" element={<DocumentListPage />} />
               <Route path="/documents/new" element={<DocumentCreatePage />} />
               <Route path="/documents/:id" element={<DocumentDetailPage />} />
               
-              {/* Document Types - VP (Veleprodaja) */}
-              <Route path="/documents/vp/ur" element={<DocumentListPage />} />
-              <Route path="/documents/vp/fo" element={<DocumentListPage />} />
-              <Route path="/documents/vp/fz" element={<DocumentListPage />} />
-              <Route path="/documents/vp/ar" element={<DocumentListPage />} />
-              <Route path="/documents/vp/pr" element={<DocumentListPage />} />
-              <Route path="/documents/vp/ro" element={<DocumentListPage />} />
-              <Route path="/documents/vp/rp" element={<DocumentListPage />} />
-              <Route path="/documents/vp/po" element={<DocumentListPage />} />
-              <Route path="/documents/vp/rv" element={<DocumentListPage />} />
-              <Route path="/documents/vp/ps" element={<DocumentListPage />} />
-              <Route path="/documents/vp/nv" element={<DocumentListPage />} />
-              <Route path="/documents/vp/kk" element={<DocumentListPage />} />
-              <Route path="/documents/vp/vs" element={<DocumentListPage />} />
-              <Route path="/documents/vp/mj" element={<DocumentListPage />} />
-              <Route path="/documents/vp/op" element={<DocumentListPage />} />
-              <Route path="/documents/vp/id" element={<DocumentListPage />} />
-              <Route path="/documents/vp/tr" element={<DocumentListPage />} />
-              <Route path="/documents/vp/pd" element={<DocumentListPage />} />
+              {/* Document Types - VP (Veleprodaja) - FIXED: Direct to form */}
+              <Route path="/documents/vp/ur" element={<DocumentCreatePage docType="UR" />} />
+              <Route path="/documents/vp/fo" element={<DocumentCreatePage docType="FO" />} />
+              <Route path="/documents/vp/fz" element={<DocumentCreatePage docType="FZ" />} />
+              <Route path="/documents/vp/ar" element={<DocumentCreatePage docType="AR" />} />
+              <Route path="/documents/vp/pr" element={<DocumentCreatePage docType="PR" />} />
+              <Route path="/documents/vp/ro" element={<DocumentCreatePage docType="RO" />} />
+              <Route path="/documents/vp/rp" element={<DocumentCreatePage docType="RP" />} />
+              <Route path="/documents/vp/po" element={<DocumentCreatePage docType="PO" />} />
+              <Route path="/documents/vp/rv" element={<DocumentCreatePage docType="RV" />} />
+              <Route path="/documents/vp/ps" element={<DocumentCreatePage docType="PS" />} />
+              <Route path="/documents/vp/nv" element={<DocumentCreatePage docType="NV" />} />
+              <Route path="/documents/vp/kk" element={<DocumentCreatePage docType="KK" />} />
+              <Route path="/documents/vp/vs" element={<DocumentCreatePage docType="VS" />} />
+              <Route path="/documents/vp/mj" element={<DocumentCreatePage docType="MJ" />} />
+              <Route path="/documents/vp/op" element={<DocumentCreatePage docType="OP" />} />
+              <Route path="/documents/vp/id" element={<DocumentCreatePage docType="ID" />} />
+              <Route path="/documents/vp/tr" element={<DocumentCreatePage docType="TR" />} />
+              <Route path="/documents/vp/pd" element={<DocumentCreatePage docType="PD" />} />
               
-              {/* Document Types - MP (Maloprodaja) */}
-              <Route path="/documents/mp/pm" element={<DocumentListPage />} />
-              <Route path="/documents/mp/psm" element={<DocumentListPage />} />
-              <Route path="/documents/mp/vsm" element={<DocumentListPage />} />
-              <Route path="/documents/mp/mjm" element={<DocumentListPage />} />
-              <Route path="/documents/mp/idm" element={<DocumentListPage />} />
-              <Route path="/documents/mp/opm" element={<DocumentListPage />} />
-              <Route path="/documents/mp/kkm" element={<DocumentListPage />} />
-              <Route path="/documents/mp/nvm" element={<DocumentListPage />} />
-              <Route path="/documents/mp/oum" element={<DocumentListPage />} />
-              <Route path="/documents/mp/oim" element={<DocumentListPage />} />
-              <Route path="/documents/mp/rmz" element={<DocumentListPage />} />
-              <Route path="/documents/mp/rpm" element={<DocumentListPage />} />
-              <Route path="/documents/mp/trm" element={<DocumentListPage />} />
-              <Route path="/documents/mp/dmk" element={<DocumentListPage />} />
+              {/* Document Types - MP (Maloprodaja) - FIXED: Direct to form */}
+              <Route path="/documents/mp/pm" element={<DocumentCreatePage docType="PM" />} />
+              <Route path="/documents/mp/psm" element={<DocumentCreatePage docType="PSM" />} />
+              <Route path="/documents/mp/vsm" element={<DocumentCreatePage docType="VSM" />} />
+              <Route path="/documents/mp/mjm" element={<DocumentCreatePage docType="MJM" />} />
+              <Route path="/documents/mp/idm" element={<DocumentCreatePage docType="IDM" />} />
+              <Route path="/documents/mp/opm" element={<DocumentCreatePage docType="OPM" />} />
+              <Route path="/documents/mp/kkm" element={<DocumentCreatePage docType="KKM" />} />
+              <Route path="/documents/mp/nvm" element={<DocumentCreatePage docType="NVM" />} />
+              <Route path="/documents/mp/oum" element={<DocumentCreatePage docType="OUM" />} />
+              <Route path="/documents/mp/oim" element={<DocumentCreatePage docType="OIM" />} />
+              <Route path="/documents/mp/rmz" element={<DocumentCreatePage docType="RMZ" />} />
+              <Route path="/documents/mp/rpm" element={<DocumentCreatePage docType="RPM" />} />
+              <Route path="/documents/mp/trm" element={<DocumentCreatePage docType="TRM" />} />
+              <Route path="/documents/mp/dmk" element={<DocumentCreatePage docType="DMK" />} />
               
               {/* Inventory */}
               <Route path="/inventory/stock" element={<ComingSoonPage title="Robna Evidencija" />} />
