53c0e1efb6e20d57b50607dd12ecb82f5232d7b4
Sasonal Dekant <sasonal.dekant@gmail.com>
2025-12-10 20:21:27 +0100

    refactor: update Document index.ts exports to include new components

--- src/components/Document/index.ts
+++ src/components/Document/index.ts
@@ -1,8 +1,17 @@
-// Document module exports
-export { DocumentHeader } from './DocumentHeader';
-export { DocumentForm } from './DocumentForm';
-export { DocumentItemsTable } from './DocumentItemsTable';
-export { DocumentCostsTable } from './DocumentCostsTable';
-export { EditableCell } from './EditableCell';
-export { ConflictDialog } from './ConflictDialog';
-export type { CellNavigationDirection } from './EditableCell';
+// Tab Navigation
+export { default as TabsComponent } from './TabsComponent';
+export type { TabConfig } from './TabsComponent';
+
+// Stavke (Items) Table
+export { default as StavkeDokumentaTable } from './StavkeDokumentaTable';
+export type { Stavka } from './StavkeDokumentaTable';
+
+// Troškovi (Costs) Table
+export { default as TroskoviTable } from './TroskoviTable';
+export type { Trosak } from './TroskoviTable';
+
+// Editable Cell Component
+export { default as EditableCell } from './EditableCell';
+
+// Dialogs
+export { default as ConflictDialog } from './ConflictDialog';
